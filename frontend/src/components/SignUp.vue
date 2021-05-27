93f753770e68690daa558edbe19269861f1d5be9
Traoré Rokia <rokia.traore@gmx.fr>
2021-05-27 02:29:08 +0200

    signup

---
+++ frontend/src/components/SignUp.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="register">
+      <input type="text" v-model="username" placeholder="Entrer votre username"/>
+      <input type="text" v-model="email" placeholder="Entrer votre Email"/>
+      <input type="password" v-model="password" placeholder="Entrer votre mot de passe"/>
+      <button v-on:click="signUp">S'enregistrer</button>>
+
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'SignUp',
+  data(){
+    return{
+      username: '',
+      email: '',
+      password:''
+    }
+
+  },
+  methods: {
+      async signUp(){
+          let result = await axios.post('http://localhost:3000/api/signup',{
+              email:this.email,
+              username:this.username,
+              password:this.password
+          });
+
+          console.log(result);
+          if(result.status == 201){
+              alert("Utilisateur enregistré")
+              localStorage.setItem('user-info', JSON.stringify(result.data))
+          }
+      }
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
---
+++ frontend/src/views/SignUp.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="homeSignUp">
+    <SignUp />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import SignUp from '@/components/SignUp.vue'
+
+export default {
+  name: 'HomeSignUp',
+  components: {
+    SignUp,
+  }
+}
+</script>
